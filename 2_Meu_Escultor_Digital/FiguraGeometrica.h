27b4ef44e92e6a8f495a269ae39b9e8524db5e79
JulioAprígio <29908653+JulioAprigio8@users.noreply.github.com>
2022-12-22 15:36:49 -0300

    implementando doxygen

--- 2_Meu_Escultor_Digital/FiguraGeometrica.h
+++ 2_Meu_Escultor_Digital/FiguraGeometrica.h
@@ -1,19 +1,15 @@
-/* Observaco: O metodo virtual void draw recebe, 
-como parametro em nosso codigo, um objeto da classe Sculptor.
-*/
 #ifndef FIGURAGEOMETRICA_H
 #define FIGURAGEOMETRICA_H
-
 #include "Sculptor.h"
 
-class FiguraGeometrica // superclasse, todas as outras irao herdar ela
-{
-    protected:
-        float r, g, b, a; // ira armazenar as cores que estao sendo trabalhadas atualmente
-    public:
-        FiguraGeometrica(){}
-        virtual ~FiguraGeometrica(){}
-        virtual void draw(Sculptor &s)=0; // metodo virtual puro, aponta para o metodo draw das outras classes
+class FiguraGeometrica{
+protected:
+  float r, g, b, a;
+public:
+  FiguraGeometrica(){};
+  virtual void draw(Sculptor &s)=0;
+  virtual ~FiguraGeometrica(){}
 };
+
 
 #endif // FIGURAGEOMETRICA_H
